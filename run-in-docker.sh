b0fab07adb3d16f424e4af3b87ed37164e2b4dc7
Mike Angstadt <mike.angstadt@wpengine.com>
2018-02-10 12:34:14 -0600

    Update run-in-docker.sh

--- run-in-docker.sh
+++ run-in-docker.sh
@@ -7,9 +7,10 @@ maven_cache_repo="${HOME}/.m2/repository"
 
 mkdir -p "${maven_cache_repo}"
 
+apt-cache policy docker-engine
 apt-get update
-apt-get install -y docker 
-service docker restart
+apt-get upgrade -y
+apt-get install -y docker-engine
 docker run --rm -it \
         -w /gen \
         -e GEN_DIR=/gen \
