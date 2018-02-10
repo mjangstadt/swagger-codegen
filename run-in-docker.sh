c3378bdc7ab41e15ad73d102437cad9be03ec46b
Mike Angstadt <mike.angstadt@wpengine.com>
2018-02-10 12:16:30 -0600

    Update run-in-docker.sh

--- run-in-docker.sh
+++ run-in-docker.sh
@@ -7,9 +7,8 @@ maven_cache_repo="${HOME}/.m2/repository"
 
 mkdir -p "${maven_cache_repo}"
 
-apt-get update 
 apt-get install -y docker 
-sudo docker run --rm -it \
+docker run --rm -it \
         -w /gen \
         -e GEN_DIR=/gen \
         -e MAVEN_CONFIG=/var/maven/.m2 \
